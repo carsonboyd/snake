d6cf3abf48080565f8b0065b58586caa99faae97
carsonboyd <s3284153@student.rmit.edu.au>
2013-04-23 01:22:55 -0700

    Todays work/ Only added player pick corner algorithm

---
+++ Item.java
@@ -0,0 +1,24 @@
+package Server;
+
+public class Item {
+
+	private int width;
+	private int height; // width/height equals location
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+}
